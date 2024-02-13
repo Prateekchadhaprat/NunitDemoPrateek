d297a03337f8771a0ed3bbc3bdc826b43b766cb6
Prateek <prateek.9n@gmail.com>
2024-02-12 19:06:24 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitDemoPrateek
+{
+    public static class Triangle
+    {  // Prateek Chadha lab3_ Mutant testing
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "This triangle is valid.";
+            }
+            else
+            {
+                result = "This triangle is NOT valid.";
+            }
+            return result;
+
+        }
+    }
+}
